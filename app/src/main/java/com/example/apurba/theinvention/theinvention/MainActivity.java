f8dec4deea3ad6c4d82afb251d5a95e645f85f2a
Apurba Biswas <apurba10024@gmail.com>
2018-04-28 13:44:27 +0600

    1.1 Added style for the app

--- app/src/main/java/com/example/apurba/theinvention/theinvention/MainActivity.java
+++ app/src/main/java/com/example/apurba/theinvention/theinvention/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.apurba.theinvention.theinvention;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpFabButton();
+    }
+
+    private void setUpFabButton(){
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Ok i am on", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
